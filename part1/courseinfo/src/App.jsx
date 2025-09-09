8aa43c49dd01fefb9a2c93a9fcbdf37194348058
Anees Sulaiman <itzanees@gmail.com>
2025-09-09 03:02:06 +0300

    Updated for Exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,47 +1,49 @@
 
 function App() {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   const Course = (props) => {
-    return(
-      <h1>{props.title}</h1>
+    return (
+      <h1>{props.title['name']}</h1>
     )
   }
-  const Part = (props) =>{
-    return(
+  const Part = (props) => {
+    return (
       <div>
-        <p>{props.content[0].name} {props.content[0].exercises}</p>
-        <p>{props.content[1].name} {props.content[1].exercises}</p>
-        <p>{props.content[2].name} {props.content[2].exercises}</p>
+        <p>{props.content.parts[0].name} {props.content.parts[0].exercises}</p>
+        <p>{props.content.parts[1].name} {props.content.parts[1].exercises}</p>
+        <p>{props.content.parts[2].name} {props.content.parts[2].exercises}</p>
       </div>
     )
   }
 
-  const Total = (props) =>{
-    return(
-       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  const Total = (props) => {
+    return (
+      <p>Number of exercises {props.parts.parts[0].exercises + props.parts.parts[1].exercises + props.parts.parts[2].exercises}</p>
     )
-      
+
   }
   return (
     <>
-      <Course title={course}/>
-      <Part content={parts} />
-      <Total parts={parts}/>
+      <Course title={course} />
+      <Part content={course} />
+      <Total parts={course} />
     </>
   )
 }
